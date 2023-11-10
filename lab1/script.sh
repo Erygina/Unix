b02dddfd87b8762bb8090df4cdc46fe20946c028
meme0w <113451606+meme0w@users.noreply.github.com>
2023-11-10 20:22:30 +0300

    2 try

--- lab1/script.sh
+++ lab1/script.sh
@@ -3,6 +3,9 @@
 # Временный каталог
 tmp_dir=$(mktemp -d)
 
+# Текущий каталог
+directory=$(pwd)
+
 # Функция очистки временного каталога
 clean_and_exit_handler()
 {
@@ -32,32 +35,25 @@ comment_name_without_extra_char="$(echo "$comment_name" | cut -d ':' -f2-)"
 # Убираем все пробелы
 output_file=$(echo "$comment_name_without_extra_char" | tr -d '[:space:]')
 
+cd "$temp"
+
+
 # 3. Компиляция исходного файла
-# Проверка типа файла 
-if [[ "$source_file" == *.tex  ]]; then
- # Компиляция исходного файла, создание выходного файла во временной директории
- pdflatex -output-directory "$tmp_dir" "$source_file"
+# Проверка типа файла
+if [[ "$source_file" == *.cpp ]]; then
+ # Компиляция исходного файла, создание выходного файла во временной директории 
+ g++ -o "$tmp_dir/$output_file" "$directory/$source_file"
  if [ $? -ne 0 ]; then
   echo "Выбранный файл не компилируется"
   clean_and_exit_handler 1
  fi
 
-elif [[ "$source_file" == *.cpp ]]; then 
- g++ -o "$tmp_dir/$output_file" "$source_file"
- if [ $? -ne 0 ]; then
-  echo "Выбранный файл не компилируется"
-  clean_and_exit_handler 1
- fi
-
-else gcc -o "$tmp_dir/$output_file" "$source_file"
+else gcc -o "$tmp_dir/$output_file" "$directory/$source_file"
  if [ $? -ne 0 ]; then
   echo "Выбранный файл не компилируется"
   clean_and_exit_handler 1
  fi
 fi
-
-# Текущая директория
-directory=$(pwd)
 
 # Перемещение файла
 mv "$tmp_dir/$output_file" "$directory"
@@ -68,10 +64,7 @@ fi
 
 echo "Успешная компиляция файла: $source_file"
 # Выведем в терминал то, что выводится в исполняемом файле
-# Стоит проверка на пустую строку, тк метод не сработает для .tex 
-if [ ! -z "$output_file" ]; then
- echo "Вывод из файла"
+echo "Вывод из файла"
  ./$output_file
-fi
 clean_and_exit_handler 0
  
